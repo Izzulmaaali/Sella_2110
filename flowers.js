e05b01f6744affac3c3ece3ba5183c3cbfc701d2
huda izzul M <ibapak984@gmail.com>
2025-10-15 06:52:37 +0700

    Update flowers.js

--- flowers.js
+++ flowers.js
@@ -3,7 +3,7 @@ onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
   
-      const titles = ('I LOVE U FEYYINA ').split('')
+      const titles = ('Fresh Flower Buat Kamu').split('')
       const titleElement = document.getElementById('title');
       let index = 0;
   
